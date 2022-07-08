17bbf62acdbb580e85b9f54ae931a19e0bd8a8d6
wa7t <445190686@qq.com>
2022-07-08 14:03:05 +0800

    Logger use condition variable

--- MiniLog.hpp
+++ MiniLog.hpp
@@ -5,13 +5,14 @@
 #include <memory>
 #include <mutex>
 #include <thread>
+#include <condition_variable>
 #include "utils.h"
 
-#define minilog(level, fmt, ...)                                                      \
-    do                                                                                \
-    {                                                                                 \
-        Logger::GetInstance()->Log(level, __FILE__, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__); \
-    } while (0)
+#define minilog(level, fmt, ...)                                                                 \
+	do                                                                                           \
+	{                                                                                            \
+		Logger::GetInstance()->Log(level, __FILE__, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__); \
+	} while (0)
 
 enum class LogLevel_e
 {
@@ -47,10 +48,10 @@ static std::string convert_level_to_string(LogLevel_e level)
 class Logger
 {
 public:
-	Logger(const Logger&) = delete;
-	Logger operator=(const Logger&) = delete;
+	Logger(const Logger &) = delete;
+	Logger operator=(const Logger &) = delete;
 	static std::shared_ptr<Logger> GetInstance();
-	void Log(LogLevel_e level, const char* file, const char* func, int line, const char* format, ...);
+	void Log(LogLevel_e level, const char *file, const char *func, int line, const char *format, ...);
 	void Work();
 
 private:
@@ -62,23 +63,25 @@ private:
 	~Logger()
 	{
 		_running = false;
+		_queueCv.notify_all();
 		_thread.join();
 	}
 	static std::shared_ptr<Logger> _inst;
 	std::mutex _queueMutex;
+	std::condition_variable _queueCv;
 	std::queue<std::string> _queue;
 	std::thread _thread;
 	bool _running;
-
 };
 
-std::shared_ptr<Logger> Logger::_inst = std::shared_ptr<Logger>(new Logger, [](Logger* p) {delete p; });
+std::shared_ptr<Logger> Logger::_inst = std::shared_ptr<Logger>(new Logger, [](Logger *p)
+																{ delete p; });
 std::shared_ptr<Logger> Logger::GetInstance()
 {
 	return _inst;
 }
 
-void Logger::Log(LogLevel_e level, const char* file, const char* func, int line, const char* format, ...)
+void Logger::Log(LogLevel_e level, const char *file, const char *func, int line, const char *format, ...)
 {
 	char variaty[1024];
 	va_list args;
@@ -88,24 +91,33 @@ void Logger::Log(LogLevel_e level, const char* file, const char* func, int line,
 	char res[2048];
 	sprintf(res, "[%s] [%s:%d - %s] %s\n", convert_level_to_string(level).c_str(), file, line, func, variaty);
 	std::string oneLog = res;
-	std::lock_guard<std::mutex> lock(_queueMutex);
-	_queue.push(std::move(oneLog));
+	{
+		std::lock_guard<std::mutex> lock(_queueMutex);
+		_queue.push(std::move(oneLog));
+	}
+	_queueCv.notify_all();
 }
 
 void Logger::Work()
 {
-	while (_running) {
-		_queueMutex.lock();
-		if (_queue.size() > 0)
-		{
-			std::string& log = _queue.front();
+	while (_running)
+	{
+		// _queueMutex.lock();
+		// while(_queue.empty())
+		// {
+		// 	_queueMutex.unlock();
+		// 	_ququeCv.wait();
+		// }
+		// _queueMutex.lock();
+		// std::string &log = _queue.front();
+		// _queue.pop();
+		// _queueMutex.unlock();
+		std::unique_lock<std::mutex> lock(_queueMutex);
+		_queueCv.wait(lock, [this](){ return !(this->_queue.empty()) || !this->_running;});
+		if(!_queue.empty()){
+			std::string &log = _queue.front();
 			printf("%s", log.c_str());
 			_queue.pop();
 		}
-		else {
-			_queueMutex.unlock();
-			sleep_ms(1);
-		}
-		_queueMutex.unlock();
 	}
 }
